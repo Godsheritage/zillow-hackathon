dc59c38b9afd2b97979eeb71864be721446d1033
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-09-11 19:20:47 -0400

    completed home page

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -4,10 +4,18 @@ import family from "../shared/images/family.jpg";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home">
       <MainNavigation />
-      {/* <img src={family} alt="family.jpg" /> */}   
-      <div className="family-page" ></div>
+      <div>
+        <div className="family-page"></div>
+        <div className=" home-text">
+          <h1 className="pb-5">
+            "Had a rough start to building credit? Try out Z-Save and grow your
+            wallet to further qualify you for your dream home"
+          </h1>
+          <button className="btn btn-primary">Try Z-Save!</button>
+        </div>
+      </div>
     </div>
   );
 };
