789f04039a989944d6e18ceab63d81e58171aba1
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-09-12 05:52:04 -0400

    updates

--- src/save/ZillowSave.tsx
+++ src/save/ZillowSave.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import './zillowSave.scss'
+import "./zillowSave.scss";
+import MainNavigation from "../shared/navigation/main navigation/MainNavigation";
+
 const ZillowSave = () => {
   return (
-    <div>ZillowSave</div>
-  )
-}
+    <div className="dashboard">
+      <MainNavigation />
+      <h1 className="h4">Total Z-Save Amount</h1>
+      <div className="d-flex ">
+        <h1 className="h5" color="#0B5ED7">$20,000</h1>
+        <button className="btn btn-primary">Manage Button</button>
+      </div>
+    </div>
+  );
+};
 
-export default ZillowSave
+export default ZillowSave;
